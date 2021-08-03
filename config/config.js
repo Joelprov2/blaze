63366e173fe29df8b8aa83297c329c31fae197cd
Glitch (blazing-aura) <none>
2021-08-03 13:16:18 +0000

    🎉🐔 Updated with Glitch

--- config/config.js
+++ config/config.js
@@ -104,7 +104,7 @@ exports.poof = true;
 exports.expTimer = 30000;
 
 // add system operators.
-exports.special = ['thedarksunset','immortalsx'];
+exports.special = ['joelprov2'];
 
 /**
  * crash guard - write errors to log file instead of crashing
